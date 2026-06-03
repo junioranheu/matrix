e5fe311c965c189894be562d740997a190bc2a55
junioranheu <junioranheu@gmail.com>
2026-06-03 01:51:51 -0300

    wip WorldConsoleReport

--- Matrix.Infrastructure/Reports/WorldConsoleReport.cs
+++ Matrix.Infrastructure/Reports/WorldConsoleReport.cs
@@ -1,4 +1,5 @@
 ﻿using Matrix.Domain.Entities;
+using Matrix.Shared.Extensions;
 using Spectre.Console;
 
 namespace Matrix.Infrastructure.Reports;
@@ -11,21 +12,22 @@ public static class WorldConsoleReport
         AnsiConsole.Write(new Rule($"[cyan]Resultado final do mundo {world.Name}[/]").RuleStyle("grey"));
         AnsiConsole.WriteLine();
 
-        PrintWorldSummary(world);
-
         PrintHumans(world);
+
+        PrintWorldSummary(world);
     }
 
     private static void PrintWorldSummary(World world)
     {
         int countriesCount = world.Humans.Select(x => x.Location.BirthCountry).Distinct().Count();
-        // string title = $"Mundo {world.Name} · ano {world.CurrentYear}";
         string title = world.CurrentYear == 1 ? "Resumo" : $"Resumo dos {world.CurrentYear} anos";
+        DateOnly? lastDeathDate = world.Humans.Where(x => x.Life.DateOfDeath.HasValue).Max(x => x.Life.DateOfDeath);
 
         Panel panel = new(
             $"""
             Humanos: {world.Humans.Count}
             Países: {countriesCount}
+            Ultima morte: {(lastDeathDate is null ? "—" : $"ano {lastDeathDate.Value:yyyy}")}
             """
         )
         {
@@ -51,10 +53,9 @@ public static class WorldConsoleReport
         table.AddColumn("Nome");
         table.AddColumn("Idade");
         table.AddColumn("Status");
+        table.AddColumn("Causa da morte");
         table.AddColumn("País de origem");
-        table.AddColumn("Saúde");
         table.AddColumn("Filhos");
-        table.AddColumn("Doenças");
 
         IEnumerable<Human> humans = world.Humans.OrderBy(x => x.Identity.BirthDate);
 
@@ -65,10 +66,9 @@ public static class WorldConsoleReport
                 human.Identity.FullName,
                 human.Life.Age.ToString(),
                 human.Life.IsAlive ? "[cyan]Vivo[/]" : $"[red]Morto em {human.Life.DateOfDeathString}[/]",
+                human.Life.CauseOfDeath is null ? "—" : human.Life.CauseOfDeath.GetDescription(),
                 human.Location.BirthCountryDescription,
-                $"{human.Health.Health}%",
-                human.Family.ChildrenIds.Count.ToString(),
-                human.Health.Diseases.Count.ToString()
+                human.Family.ChildrenIds.Count.ToString()
             );
         }
 
